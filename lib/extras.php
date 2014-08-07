fed83a39dbf5722760566758ac4016484ab87a36
Philly <philneal@philneal.com>
2014-08-07 09:35:34 -0300

    Day 2 updates

--- lib/extras.php
+++ lib/extras.php
@@ -20,3 +20,23 @@ function roots_wp_title($title) {
   return $title;
 }
 add_filter('wp_title', 'roots_wp_title', 10);
+add_theme_support( 'buddypress' );
+
+// clean up unwanted JS scripts
+function my_print_scripts(){
+   if ( !is_admin() ) {
+      wp_deregister_script('jquery');
+      wp_deregister_script('ai1ec_requirejs');
+   }
+}
+add_action('wp_print_scripts', 'my_print_scripts');
+
+// clean up unwanted CSS
+function my_print_css() {
+   if ( !is_admin() ) {
+      wp_deregister_style('ai1ec-render_css');
+      wp_deregister_style('ai1ec-event');
+      wp_deregister_style('ai1ec-calendar');
+   }
+}
+add_action('wp_print_styles', 'my_print_css');
